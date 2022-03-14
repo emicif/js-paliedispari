1ba6a58a20f974c65237be2f4c5fe115876b0d43
emicif <emanuela.cifolelli@gmail.com>
2022-03-14 17:44:21 +0100

    Creazione due cartelle

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -14,49 +14,6 @@ sommaNumeri (2, 6);
 
 
 
-/* ESERCIZIO 1
-Chiedere all’utente di inserire una parola
-Creare una funzione per capire se la parola inserita è palindroma
-*/
-
-// chiedo con prompt utente di inserire parola
-// leggo parola inserita dall'utente
-// chiedo a js di leggere la parola al contrario
-
-
-
-
-// chiedo con all'utente di inserire parola
-let parolaUtente = prompt('Inserisci una parola palindroma');
-
-
-// scompongo la parola utente per avere gli elementi
-let parolaDivisa = parolaUtente.split('');
-
-
-// vado a capire se la parola inversa è uguale al contrario della parolaDivisa
-let parolaInversaDivisa = parolaDivisa.reverse('');
-
-
-// a questo punto devo rimettere insieme gli elementi suddivisi prima con split
-let parolaInvertita = parolaInversaDivisa.join('');
-
-
-// se uguale stampo "palindroma"
-// altrimenti stampo "non palindroma"
-if (parolaUtente === parolaInvertita) {
-    console.log('La parola inserita è palindroma')
-} else {
-    console.log('La parola inserita NON è palindroma');
-}
-
-
-// preparo la funzione
-function parolaPalindroma (parolaUtente, parolaInvertita) {
-    let risultato = (parolaUtente === parolaInvertita);
-    return risultato;
-}
-
 
 
 /* ESERCIZIO 2
