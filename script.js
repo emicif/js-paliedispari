859a5432db53dae54f72296691b40183a0df3b43
emicif <emanuela.cifolelli@gmail.com>
2022-03-14 14:34:28 +0100

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log('JS OK');
+
+
+function miafunzione() {
+    //blocco di codice
+}
+
+function sommaNumeri (num1, num2) {
+    alert (num1 + num2);
+}
+
+sommaNumeri (2, 6);
+
+
+
+
+/* Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
